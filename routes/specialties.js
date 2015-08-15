2ae2a7f5afab3b5d3096b53bfd5177adff76b15d
kristenmills <kristen@kristen-mills.com>
2015-08-15 11:20:55 -0700

    specialties and tags

---
+++ routes/specialties.js
@@ -0,0 +1,24 @@
+'use strict';
+
+import { Router } from 'express';
+import Specialty from '../models/specialty';
+import scopify from '../helpers/scopify';
+import paginate from '../middleware/paginate';
+
+const router = Router(); // eslint-disable-line new-cap
+
+router
+  .route('/')
+    .get(paginate, (req, res, next) => {
+      Specialty
+        .findAndCountAll()
+        .then(result => res.send({
+          total: result.count,
+          perPage: req.query.perPage,
+          currentPage: req.query.page,
+          data: result.rows,
+        }))
+        .catch(err => next(err));
+    });
+
+export default router;
