a1400d04fdcfa8a97dabe45ae6329d67b9f6b47d
kristenmills <kristen@kristen-mills.com>
2015-08-17 10:35:20 -0700

    Added ical support

---
+++ helpers/ical.js
@@ -0,0 +1,44 @@
+'use strict';
+
+import moment from 'moment';
+
+const timezone = `BEGIN:VTIMEZONE
+TZID:America/New_York
+X-LIC-LOCATION:America/New_York
+BEGIN:DAYLIGHT
+TZOFFSETFROM:-0500
+TZOFFSETTO:-0400
+TZNAME:EDT
+DTSTART:19700308T020000
+RRULE:FREQ=YEARLY;BYMONTH=3;BYDAY=2SU
+END:DAYLIGHT
+BEGIN:STANDARD
+TZOFFSETFROM:-0400
+TZOFFSETTO:-0500
+TZNAME:EST
+DTSTART:19701101T020000
+RRULE:FREQ=YEARLY;BYMONTH=11;BYDAY=1SU
+END:STANDARD
+END:VTIMEZONE`;
+
+function dateString(date) {
+  return moment(date).format('YYYYMMDD[T]HHmmss');
+}
+
+function event(ev) {
+  return `BEGIN:VEVENT
+DTEND;TZID=America/New_York;VALUE=DATE-TIME:${dateString(ev.endDate)}
+DTSTART;TZID=America/New_York;VALUE=DATE-TIME:${dateString(ev.startDate)}
+DESCRIPTION:${ev.description}
+SUMMARY:${ev.name}
+LOCATION:${ev.location}
+END:VEVENT`;
+}
+
+export default function ical(events) {
+  return `BEGIN:VCALENDAR
+CALSCALE:GREGORIAN
+${timezone}
+${events.map(event).join('\n')}
+END:VCALENDAR`;
+}
--- routes/events.js
+++ routes/events.js
@@ -5,6 +5,7 @@ import Event from '../models/event';
 import scopify from '../helpers/scopify';
 import { needs } from '../middleware/permissions';
 import paginate from '../middleware/paginate';
+import ical from '../helpers/ical';
 
 const router = Router(); // eslint-disable-line new-cap
 
@@ -12,15 +13,27 @@ router
   .route('/')
     .get(paginate, (req, res, next) => {
       const scopes = scopify(req.query, 'name', 'committee', 'location', 'between', 'before', 'after', 'featured');
-      Event.scope(scopes)
-        .findAndCountAll()
-        .then(result => res.send({
-          total: result.count,
-          perPage: req.query.perPage,
-          currentPage: req.query.page,
-          data: result.rows,
-        }))
-        .catch(err => next(err));
+      if (req.accepts('json')) {
+        Event
+          .scope(scopes)
+          .findAndCountAll()
+          .then(result => res.send({
+            total: result.count,
+            perPage: req.query.perPage,
+            currentPage: req.query.page,
+            data: result.rows,
+          }))
+          .catch(err => next(err));
+      } else if (req.accepts('ics')) {
+        scopes.shift();
+        return Event
+          .scope(scopes)
+          .findAll()
+          .then(events => res.send(ical(events)))
+          .catch(err => next(err));
+      } else {
+        return next({ status: 406, message: `${req.headers.accept} is not acceptable` });
+      }
     })
     .post(needs('events', 'create'), (req, res, next) => {
       Event.create(req.body, { fields: ['name', 'startDate', 'endDate', 'description', 'location', 'image', 'committeeId'] })
