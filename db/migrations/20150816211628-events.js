f2efdec0d80d6396ff1a8b862e2facc8b482c5fc
kristenmills <kristen@kristen-mills.com>
2015-08-16 21:46:48 -0700

    Events migrations

---
+++ db/migrations/20150816211628-events.js
@@ -0,0 +1,40 @@
+'use strict';
+
+export function up(queryInterface, Sequelize) {
+  return queryInterface.createTable('events', {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+    },
+    startDate: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+    endDate: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+    description: Sequelize.TEXT,
+    location: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    image: Sequelize.STRING,
+    committeeId: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      references: { model: 'committee', key: 'id' },
+    },
+    createdAt: Sequelize.DATE,
+    updatedAt: Sequelize.DATE,
+  });
+}
+
+export function down(queryInterface) {
+  return queryInterface.dropTable('events');
+}
