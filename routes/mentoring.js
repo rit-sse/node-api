bbf77cc41fa631da885fd8d59d1c54dc33af0666
kristenmills <kristen@kristen-mills.com>
2015-08-18 14:40:54 -0700

    Changed routes and stuff

--- routes/mentoring.js
+++ routes/mentoring.js
@@ -4,11 +4,13 @@ import { Router } from 'express';
 import headcounts from './headcounts';
 import mentors from './mentors';
 import shifts from './shifts';
+import specialties from './specialties';
 
 const router = Router(); // eslint-disable-line new-cap
 
 router.use('/headcounts', headcounts);
 router.use('/mentors', mentors);
 router.use('/shifts', shifts);
+router.use('/specialties', specialties);
 
 export default router;
---
+++ routes/qdb.js
@@ -0,0 +1,12 @@
+'use strict';
+
+import { Router } from 'express';
+import quotes from './quotes';
+import tags from './tags';
+
+const router = Router(); // eslint-disable-line new-cap
+
+router.use('/quotes', quotes);
+router.use('/tags', tags);
+
+export default router;
