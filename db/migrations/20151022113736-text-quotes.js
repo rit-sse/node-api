017c4eab3ef8a805c33c3ef9d544c7fffd8040ab
Kristen Mills <kristen@kristen-mills.com>
2015-11-04 11:35:25 -0500

    fix migration

--- db/migrations/20151022113736-text-quotes.js
+++ db/migrations/20151022113736-text-quotes.js
@@ -1,27 +1,27 @@
 'use strict';
 
 export function up(queryInterface, Sequelize) {
-  return Promise.all([
-    queryInterface.changeColumn('quotes', 'body', {
+  return queryInterface.changeColumn('quotes', 'body', {
+    type: Sequelize.TEXT,
+    allowNull: false,
+    unique: true,
+  })
+  .then( () => {
+    return queryInterface.changeColumn('quotes', 'description', {
       type: Sequelize.TEXT,
-      allowNull: false,
-      unique: true,
-    }),
-    queryInterface.changeColumn('quotes', 'description', {
-      type: Sequelize.TEXT,
-    }),
-  ]);
+    });
+  });
 }
 
 export function down(queryInterface) {
-  return Promise.all([
-    queryInterface.changeColumn('quotes', 'body', {
+  return queryInterface.changeColumn('quotes', 'body', {
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: true,
+  })
+  .then( () => {
+    return queryInterface.changeColumn('quotes', 'description', {
       type: Sequelize.STRING,
-      allowNull: false,
-      unique: true,
-    }),
-    queryInterface.changeColumn('quotes', 'description', {
-      type: Sequelize.STRING,
-    }),
-  ]);
+    });
+  });
 }
