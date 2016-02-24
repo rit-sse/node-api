0bc7fd8b286596a0ce8d9b512d7ad136604288d2
Michael Timbrook <timbrook480@gmail.com>
2016-02-23 22:53:13 -0500

    Updated entrypoint to enforce some run practice

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,31 @@
 #!/bin/bash
-mkdir keys
-npm run keygen
-npm run bootstrap
+# Check and make sure we have keys, if not make them
+if [[ ! -e keys/public.key && ! -e keys/public.key ]];
+then
+  echo """
+  Keys were not mounted into the container, this is ok for development but when
+  running in production we don't want to blow away these keys everytime.
+  Consider mounting in <your/path/to/keys>:/app/keys
+  """
+  mkdir keys
+  npm run keygen
+fi;
+
+# Production vs dev requirements
+if [[ "$NODE_ENV" = "production" ]];
+then
+  echo """
+  Running in production, just checking a few things
+  """
+  if [[ ! -e config/database/configs/production.json ]];
+  then
+  echo """
+  WARNING: no production database config found, consider mounting in the config
+  directory from the host where that config lives. UNSETTING NODE_ENV!!!
+  """
+  unset NODE_ENV;
+  fi;
+fi;
+
+npm run bootstrap --admin:firstName Michael --admin:lastName Timbrook --admin:dce mpt2360
 npm start
