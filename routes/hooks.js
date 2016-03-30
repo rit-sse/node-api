30e11e825810a7e6dc7b82ae5f6a68752a6a14cf
Michael Timbrook <timbrook480@gmail.com>
2016-03-30 18:49:14 -0400

    docker hub requires callbacks

--- routes/hooks.js
+++ routes/hooks.js
@@ -1,18 +1,40 @@
 import redis from 'redis';
 import { Router } from 'express';
 import nconf from '../config';
+import request from 'request-promise';
 
 const router = Router(); // eslint-disable-line new-cap
 
 let client = redis.createClient('redis://' + nconf.get('REDIS_PORT_6379_TCP_ADDR') + ':6379');
 
 router
+  .route('/dockerbuilds')
+    .post((req, res) => {
+      // Publish the bots message to dockerbuilds
+      client.publish('dockerbuilds', 'build for ' + req.body.repository.name + ' has finished',
+        (err, response) => {
+          res.send({ msg: "pushed to " + response + " subscribers" });
+        });  
+        // Required response for docker hub to mark the webhook as completed.
+        const responseData = {
+          "state":        "success",
+          "description":  "SSE webhook response",
+          "context":      "sse",
+          "target_url":   "https://sse.rit.edu"
+        };
+        request.post({ url: req.body.callback_url, json: responseData })
+        .then( () => console.log('Responsed') )
+        .catch( err => console.error(err) );
+    });
+
+router
   .route('/:channel')
     .post((req, res) => {
-      client.publish(req.params.channel, 'build for ' + req.body.repository.name + ' has finished',
-      (err, response) => {
-        res.send({ msg: "pushed to " + response + " subscribers" });
-      });  
+      // Publish the bots message to dockerbuilds 
+      client.publish(req.params.channel, JSON.stringify(req.body),
+        (err, response) => {
+          res.send('ok');
+        });  
     });
 
 export default router;
