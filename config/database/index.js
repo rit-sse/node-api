bab80fac5f83ca854f91566b4a85f00d943a0704
Michael Timbrook <timbrook480@gmail.com>
2017-01-23 01:17:02 -0500

    Added docker prod config for postgres

--- config/database/index.js
+++ config/database/index.js
@@ -1,11 +1,22 @@
 import fs from 'fs';
 
+// Build postgres connection string from environment
+const USER = process.env.PG_ENV_POSTGRES_USER || 'postgres';
+const PASSWORD = process.env.PG_ENV_POSTGRES_PASSWORD;
+const DB = process.env.pg_ENV_POSTGRES_DB || USER;
+const PG_CONN_STRING =
+  `postgres://${USER}:${PASSWORD}@pg:5432/dbname`;
+
 const development =  fs.existsSync('config/database/configs/development.json') // eslint-disable-line no-sync
   ? JSON.parse(fs.readFileSync('config/database/configs/development.json')) // eslint-disable-line no-sync
   : null;
+
 const production =  fs.existsSync('config/database/configs/production.json') // eslint-disable-line no-sync
   ? JSON.parse(fs.readFileSync('config/database/configs/production.json')) // eslint-disable-line no-sync
-  : null;
+  : {
+    "dialect": "postgres",
+    "host": PG_CONN_STRING
+  };
 
 const test =  fs.existsSync('config/database/configs/test.json') // eslint-disable-line no-sync
   ? JSON.parse(fs.readFileSync('config/database/configs/test.json')) // eslint-disable-line no-sync
