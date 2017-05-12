18312bdbffca5370ed1b42742b03175712de1fa7
Michael Timbrook <timbrook480@gmail.com>
2017-05-11 21:57:45 -0400

    Revert "Testing out removing pagination from Committees"
    This reverts commit 6548533d20516b8cf9705db08cd7bb50bff5cf59.

--- models/committee.js
+++ models/committee.js
@@ -1,5 +1,6 @@
 import sequelize from '../config/sequelize';
 import DataTypes from 'sequelize';
+import paginate from '../helpers/paginate';
 import Officer from './officer';
 
 export default sequelize.define('committees', {
@@ -24,5 +25,6 @@ export default sequelize.define('committees', {
         }],
       };
     },
+    paginate,
   },
 });
