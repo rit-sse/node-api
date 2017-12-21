765700aab67095e36651046cb4690e37688eb506
Bill Dybas <wmdybas@gmail.com>
2017-12-21 18:31:42 -0500

    Add Some Auth Tests

--- test/routes/auth.js
+++ test/routes/auth.js
@@ -6,23 +6,101 @@ import request from 'supertest';
 
 import app from '../../app';
 import nconf from '../../config';
+import { token } from '../helpers';
 
 describe('INTEGRATION TESTS: AUTH', function () {
   describe('GET /', function () {
-    it('', function () {
-      return false;
+    it('Denies Unauthenticated Users', function (done) {
+      const expected = {
+        error: 'not logged in',
+      };
+
+      request(app)
+        .get('/api/v2/auth')
+        .expect(401)
+        .then((response) => {
+          expect(response.body).to.deep.equal(expected);
+          done();
+        });
+    });
+
+    it('Gets the Current Logged In User', function (done) {
+      const expected = {
+        firstName: 'Test',
+        lastName: 'User',
+        dce: 'abc1234',
+      };
+
+      request(app)
+        .get('/api/v2/auth')
+        .set('Authorization', `Bearer ${token}`)
+        .expect(200)
+        .then((response) => {
+          expect(response.body).to.deep.equal(expected);
+          done();
+        });
     });
   });
 
   describe('GET /googleClientID', function () {
-    it('', function () {
-      return false;
+    it('Gets the Google Client ID', function (done) {
+      const expected = {
+        token: nconf.get('auth').google.id,
+      };
+
+      request(app)
+        .get('/api/v2/auth/googleClientID')
+        .expect(200)
+        .then((response) => {
+          expect(response.body).to.deep.equal(expected);
+          done();
+        });
     });
   });
 
   describe('POST /:provider', function () {
-    it('', function () {
-      return false;
+    it('Sends a Refresh Token', function (done) {
+      request(app)
+        .post('/api/v2/auth/refresh')
+        .set('Authorization', `Bearer ${token}`)
+        .expect(200)
+        .then((response) => {
+          // There should be a token property, but it should
+          // not be the same as the original token.
+          expect(response.body.token).to.not.be.undefined; // eslint-disable-line no-unused-expressions
+          expect(response.body).to.not.deep.equal({ token });
+          done();
+        });
+    });
+
+    it('Does Not Support Unknown Providers', function (done) {
+      const expected = {
+        error: 'invalid provider',
+      };
+
+      request(app)
+        .post('/api/v2/auth/unknown')
+        .set('Authorization', `Bearer ${token}`)
+        .expect(401)
+        .then((response) => {
+          expect(response.body).to.deep.equal(expected);
+          done();
+        });
+    });
+
+    it('Verifies a Google User', function (done) {
+      expect(false).to.equal(true);
+      done();
+    });
+
+    it('Verifies a Slack User', function (done) {
+      expect(false).to.equal(true);
+      done();
+    });
+
+    it('Does Not Verify Incorrect Credentials', function (done) {
+      expect(false).to.equal(true);
+      done();
     });
   });
 });
