5ad922529fa9aa18387988c6ab2e2c2b69d8d3f0
Bill Dybas <wmdybas@gmail.com>
2017-12-21 16:20:09 -0500

    Add Links Test Skeleton

---
+++ test/routes/links.js
@@ -0,0 +1,46 @@
+/* eslint-env mocha */
+/* eslint-disable func-names, prefer-arrow-callback  */
+
+import { expect } from 'chai';
+import request from 'supertest';
+
+import app from '../../app';
+import nconf from '../../config';
+
+describe('INTEGRATION TESTS: LINKS', function () {
+  describe('GET /', function () {
+    it('', function () {
+      return false;
+    });
+  });
+
+  describe('POST /', function () {
+    it('', function () {
+      return false;
+    });
+  });
+
+  describe('GET /go/:shortlink', function () {
+    it('', function () {
+      return false;
+    });
+  });
+
+  describe('GET /:shortlink', function () {
+    it('', function () {
+      return false;
+    });
+  });
+
+  describe('PUT /:shortlink', function () {
+    it('', function () {
+      return false;
+    });
+  });
+
+  describe('DELETE /:shortlink', function () {
+    it('', function () {
+      return false;
+    });
+  });
+});
