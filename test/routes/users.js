a82810d146cdf30867c3764651970e6a4b47752d
Bill Dybas <wmdybas@gmail.com>
2017-12-21 15:24:36 -0500

    Add Users Test Skeleton

---
+++ test/routes/users.js
@@ -0,0 +1,28 @@
+/* eslint-env mocha */
+/* eslint-disable func-names, prefer-arrow-callback  */
+
+import { expect } from 'chai';
+import request from 'supertest';
+
+import app from '../../app';
+import nconf from '../../config';
+
+describe('INTEGRATION TESTS: USERS', function () {
+  describe('GET /', function () {
+    it('', function () {
+      return false;
+    });
+  });
+
+  describe('GET /:dce', function () {
+    it('', function () {
+      return false;
+    });
+  });
+
+  describe('PUT /:dce', function () {
+    it('', function () {
+      return false;
+    });
+  });
+});
