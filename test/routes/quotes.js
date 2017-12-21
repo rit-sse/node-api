445a76e06af3c146aee5ce9247c86b801938ea44
Bill Dybas <wmdybas@gmail.com>
2017-12-21 16:14:36 -0500

    Add Quotes Test Skeleton

---
+++ test/routes/quotes.js
@@ -0,0 +1,40 @@
+/* eslint-env mocha */
+/* eslint-disable func-names, prefer-arrow-callback  */
+
+import { expect } from 'chai';
+import request from 'supertest';
+
+import app from '../../app';
+import nconf from '../../config';
+
+describe('INTEGRATION TESTS: QUOTES', function () {
+  describe('GET /', function () {
+    it('', function () {
+      return false;
+    });
+  });
+
+  describe('POST /', function () {
+    it('', function () {
+      return false;
+    });
+  });
+
+  describe('GET /:id', function () {
+    it('', function () {
+      return false;
+    });
+  });
+
+  describe('PUT /:id', function () {
+    it('', function () {
+      return false;
+    });
+  });
+
+  describe('DELETE /:id', function () {
+    it('', function () {
+      return false;
+    });
+  });
+});
