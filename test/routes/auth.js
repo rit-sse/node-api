d341fcad7315ba657ed769773c819e782745b550
Bill Dybas <wmdybas@gmail.com>
2017-12-21 16:32:39 -0500

    Add Auth Test Skeleton

---
+++ test/routes/auth.js
@@ -0,0 +1,28 @@
+/* eslint-env mocha */
+/* eslint-disable func-names, prefer-arrow-callback  */
+
+import { expect } from 'chai';
+import request from 'supertest';
+
+import app from '../../app';
+import nconf from '../../config';
+
+describe('INTEGRATION TESTS: AUTH', function () {
+  describe('GET /', function () {
+    it('', function () {
+      return false;
+    });
+  });
+
+  describe('GET /googleClientID', function () {
+    it('', function () {
+      return false;
+    });
+  });
+
+  describe('POST /:provider', function () {
+    it('', function () {
+      return false;
+    });
+  });
+});
