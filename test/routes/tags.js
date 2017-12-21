66450c658ba262399a9b41ef1c5ce53a59e27dad
Bill Dybas <wmdybas@gmail.com>
2017-12-21 16:21:59 -0500

    Add Tags Test Skeleton

---
+++ test/routes/tags.js
@@ -0,0 +1,16 @@
+/* eslint-env mocha */
+/* eslint-disable func-names, prefer-arrow-callback  */
+
+import { expect } from 'chai';
+import request from 'supertest';
+
+import app from '../../app';
+import nconf from '../../config';
+
+describe('INTEGRATION TESTS: TAGS', function () {
+  describe('GET /', function () {
+    it('', function () {
+      return false;
+    });
+  });
+});
