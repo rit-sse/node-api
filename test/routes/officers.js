1c1302aaa36fe6aee779d618dcde0e803c3315bd
Bill Dybas <wmdybas@gmail.com>
2017-12-22 16:11:56 -0500

    Add Officers Test Stubs

--- test/routes/officers.js
+++ test/routes/officers.js
@@ -9,32 +9,121 @@ import nconf from '../../config';
 
 describe('INTEGRATION TESTS: OFFICERS', function () {
   describe('GET /', function () {
-    it('', function () {
+    it('Gets Officers', function () {
       return false;
+    });
+
+    it('Filters by Title', function (done) {
+      expect(false).to.equal(true);
+      done();
+    });
+
+    it('Filters by Email', function (done) {
+      expect(false).to.equal(true);
+      done();
+    });
+
+    it('Filters by User', function (done) {
+      expect(false).to.equal(true);
+      done();
+    });
+
+    it('Filters by Primary', function (done) {
+      expect(false).to.equal(true);
+      done();
+    });
+
+    it('Filters by Committee', function (done) {
+      expect(false).to.equal(true);
+      done();
+    });
+
+    it('Filters by Active', function (done) {
+      expect(false).to.equal(true);
+      done();
     });
   });
 
   describe('POST /', function () {
-    it('', function () {
-      return false;
+    it('Requires Authentication', function (done) {
+      expect(false).to.equal(true);
+      done();
+    });
+
+    it('Requires Correct Permissions', function (done) {
+      expect(false).to.equal(true);
+      done();
+    });
+
+    it('Creates a New Committee if there is No Officer belonging to that Committee', function (done) {
+      expect(false).to.equal(true);
+      done();
+    });
+
+    it('Creates a an Officer and assigns them to an Existing Committee', function (done) {
+      expect(false).to.equal(true);
+      done();
+    });
+
+    it('Errors When Insufficient Fields Provided', function (done) {
+      expect(false).to.equal(true);
+      done();
     });
   });
 
   describe('GET /:id', function () {
-    it('', function () {
-      return false;
+    it('Gets a Specific Officer', function (done) {
+      expect(false).to.equal(true);
+      done();
+    });
+
+    it('Does Not Find a Non-existent Officer', function (done) {
+      expect(false).to.equal(true);
+      done();
     });
   });
 
   describe('PUT /:id', function () {
-    it('', function () {
-      return false;
+    it('Requires Authentication', function (done) {
+      expect(false).to.equal(true);
+      done();
+    });
+
+    it('Requires Correct Permissions', function (done) {
+      expect(false).to.equal(true);
+      done();
+    });
+
+    it('Updates a Specific Officer', function (done) {
+      expect(false).to.equal(true);
+      done();
+    });
+
+    it('Does Not Find and Update a Non-existent Officer', function (done) {
+      expect(false).to.equal(true);
+      done();
     });
   });
 
   describe('DELETE /:id', function () {
-    it('', function () {
-      return false;
+    it('Requires Authentication', function (done) {
+      expect(false).to.equal(true);
+      done();
+    });
+
+    it('Requires Correct Permissions', function (done) {
+      expect(false).to.equal(true);
+      done();
+    });
+
+    it('Deletes a Specific Officer', function (done) {
+      expect(false).to.equal(true);
+      done();
+    });
+
+    it('Does Not Find and Delete a Non-existent Officer', function (done) {
+      expect(false).to.equal(true);
+      done();
     });
   });
 });
