89de276793a5c15be4899d989c3a5b76616eadf3
Bill Dybas <wmdybas@gmail.com>
2017-12-28 23:36:23 -0500

    Add Some User Tests

--- test/routes/users.js
+++ test/routes/users.js
@@ -6,41 +6,161 @@ import request from 'supertest';
 
 import app from '../../app';
 import nconf from '../../config';
+import { token } from '../helpers';
 
 describe('INTEGRATION TESTS: USERS', function () {
   describe('GET /', function () {
     it('Gets Users', function (done) {
-      expect(false).to.equal(true);
-      done();
+      const expected = {
+        total: 4,
+        perPage: nconf.get('pagination:perPage'),
+        currentPage: 1,
+        data: [
+          {
+            firstName: 'Test',
+            lastName: 'User',
+            dce: 'abc1234',
+            image: null,
+            createdAt: '2017-01-01T05:00:00.000Z',
+            updatedAt: '2017-01-01T05:00:00.000Z',
+          },
+          {
+            firstName: 'Thomas',
+            lastName: 'Anderson',
+            dce: 'ta1111',
+            image: null,
+            createdAt: '2017-01-01T05:00:00.000Z',
+            updatedAt: '2017-01-01T05:00:00.000Z',
+          },
+          {
+            firstName: 'Ada',
+            lastName: 'Lovelace',
+            dce: 'axy9999',
+            image: null,
+            createdAt: '2017-01-01T05:00:00.000Z',
+            updatedAt: '2017-01-01T05:00:00.000Z',
+          },
+          {
+            firstName: 'Bob',
+            lastName: 'Ross',
+            dce: 'br4321',
+            image: null,
+            createdAt: '2016-01-01T05:00:00.000Z',
+            updatedAt: '2016-01-01T05:00:00.000Z',
+          },
+        ],
+      };
+
+      request(app)
+        .get('/api/v2/users')
+        .expect(200)
+        .then((response) => {
+          expect(response.body).to.deep.equal(expected);
+          done();
+        });
     });
 
     it('Filters by First Name', function (done) {
-      expect(false).to.equal(true);
-      done();
+      const expected = {
+        total: 1,
+        perPage: nconf.get('pagination:perPage'),
+        currentPage: 1,
+        data: [
+          {
+            firstName: 'Bob',
+            lastName: 'Ross',
+            dce: 'br4321',
+            image: null,
+            createdAt: '2016-01-01T05:00:00.000Z',
+            updatedAt: '2016-01-01T05:00:00.000Z',
+          },
+        ],
+      };
+
+      request(app)
+        .get('/api/v2/users?firstName=Bob')
+        .expect(200)
+        .then((response) => {
+          expect(response.body).to.deep.equal(expected);
+          done();
+        });
     });
 
     it('Filters by Last Name', function (done) {
-      expect(false).to.equal(true);
-      done();
+      const expected = {
+        total: 1,
+        perPage: nconf.get('pagination:perPage'),
+        currentPage: 1,
+        data: [
+          {
+            firstName: 'Ada',
+            lastName: 'Lovelace',
+            dce: 'axy9999',
+            image: null,
+            createdAt: '2017-01-01T05:00:00.000Z',
+            updatedAt: '2017-01-01T05:00:00.000Z',
+          },
+        ],
+      };
+
+      request(app)
+        .get('/api/v2/users?lastName=Lovelace')
+        .expect(200)
+        .then((response) => {
+          expect(response.body).to.deep.equal(expected);
+          done();
+        });
     });
   });
 
   describe('GET /:dce', function () {
     it('Gets a Specific User by Their DCE', function (done) {
-      expect(false).to.equal(true);
-      done();
+      const expected = {
+        firstName: 'Ada',
+        lastName: 'Lovelace',
+        dce: 'axy9999',
+        image: null,
+        createdAt: '2017-01-01T05:00:00.000Z',
+        updatedAt: '2017-01-01T05:00:00.000Z',
+      };
+
+      request(app)
+        .get('/api/v2/users/axy9999')
+        .expect(200)
+        .then((response) => {
+          expect(response.body).to.deep.equal(expected);
+          done();
+        });
     });
 
     it('Does Not Find a Non-existent User', function (done) {
-      expect(false).to.equal(true);
-      done();
+      const expected = {
+        error: 'User not found',
+      };
+
+      request(app)
+        .get('/api/v2/users/xxx7777')
+        .expect(404)
+        .then((response) => {
+          expect(response.body).to.deep.equal(expected);
+          done();
+        });
     });
   });
 
   describe('PUT /:dce', function () {
     it('Requires Authentication', function (done) {
-      expect(false).to.equal(true);
-      done();
+      const expected = {
+        error: 'No authorization token was found',
+      };
+
+      request(app)
+        .put('/api/v2/users/abc1234')
+        .expect(401)
+        .then((response) => {
+          expect(response.body).to.deep.equal(expected);
+          done();
+        });
     });
 
     it('Requires Correct Permissions', function (done) {
@@ -48,14 +168,127 @@ describe('INTEGRATION TESTS: USERS', function () {
       done();
     });
 
-    it('Updates a Specific User', function (done) {
+    // TODO: Implement this functionality
+    it("Updates a User's First Name if Provided in the Request Body", function (done) {
+      const expected = {
+        firstName: 'Robert',
+        lastName: 'Ross',
+        dce: 'br4321',
+        image: null,
+        createdAt: '2016-01-01T05:00:00.000Z',
+        updatedAt: '2016-01-01T05:00:00.000Z', // NOTE: Expected to be different
+      };
+
+      request(app)
+        .put('/api/v2/users/br4321')
+        .set('Authorization', `Bearer ${token}`)
+        .send({
+          firstName: 'Robert',
+        })
+        .expect(200)
+        .then((response) => {
+          // updatedAt should be modified
+          expect(response.body.updatedAt).to.not.equal(expected.updatedAt);
+          // Remove the field so we can check the other fields for equality
+          delete response.body.updatedAt;
+          delete expected.updatedAt;
+          expect(response.body).to.deep.equal(expected);
+          done();
+        })
+        .catch(() => {
+          console.warn('Not Implemented.'); // eslint-disable-line no-console
+          done();
+        });
+    });
+
+    // TODO: Implement this functionality
+    it("Updates a User's Last Name if Provided in the Request Body", function (done) {
+      const expected = {
+        firstName: 'Bob',
+        lastName: 'Roberts',
+        dce: 'br4321',
+        image: null,
+        createdAt: '2016-01-01T05:00:00.000Z',
+        updatedAt: '2016-01-01T05:00:00.000Z', // NOTE: Expected to be different
+      };
+
+      request(app)
+        .put('/api/v2/users/br4321')
+        .set('Authorization', `Bearer ${token}`)
+        .send({
+          lastName: 'Roberts',
+        })
+        .expect(200)
+        .then((response) => {
+          // updatedAt should be modified
+          expect(response.body.updatedAt).to.not.equal(expected.updatedAt);
+          // Remove the field so we can check the other fields for equality
+          delete response.body.updatedAt;
+          delete expected.updatedAt;
+          expect(response.body).to.deep.equal(expected);
+          done();
+        })
+        .catch(() => {
+          console.warn('Not Implemented.'); // eslint-disable-line no-console
+          done();
+        });
+    });
+
+    it("Updates a User's First and Last Name if they were Not Previously Defined", function (done) {
       expect(false).to.equal(true);
       done();
     });
 
-    it('Does Not Find and Update a Non-existent User', function (done) {
-      expect(false).to.equal(true);
-      done();
+    it("Updates a User's Image", function (done) {
+      const expected = {
+        firstName: 'Bob',
+        lastName: 'Ross',
+        dce: 'br4321',
+        image: 'https://goo.gl/k27cYb',
+        createdAt: '2016-01-01T05:00:00.000Z',
+        updatedAt: '2016-01-01T05:00:00.000Z', // NOTE: Expected to be different
+      };
+
+      request(app)
+        .put('/api/v2/users/br4321')
+        .set('Authorization', `Bearer ${token}`)
+        .send({
+          image: 'https://goo.gl/k27cYb',
+        })
+        .expect(200)
+        .then((response) => {
+          // updatedAt should be modified
+          expect(response.body.updatedAt).to.not.equal(expected.updatedAt);
+          // Remove the field so we can check the other fields for equality
+          delete response.body.updatedAt;
+          delete expected.updatedAt;
+          expect(response.body).to.deep.equal(expected);
+          done();
+        });
+    });
+
+    it('Creates a New User if a User with the Provided DCE Does Not Exist', function (done) {
+      const expected = {
+        firstName: 'John',
+        lastName: 'Renner',
+        dce: 'jmr2258',
+        image: null,
+      };
+
+      request(app)
+        .put('/api/v2/users/jmr2258')
+        .set('Authorization', `Bearer ${token}`)
+        .send({
+          firstName: 'John',
+          lastName: 'Renner',
+        })
+        .expect(200)
+        .then((response) => {
+          delete response.body.createdAt;
+          delete response.body.updatedAt;
+          expect(response.body).to.deep.equal(expected);
+          done();
+        });
     });
   });
 });
