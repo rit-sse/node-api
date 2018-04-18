e2d515ae19add0e5ff7304dfd645dee529b0d081
Bill Dybas <wmdybas@gmail.com>
2018-04-18 17:56:43 -0400

    Set Default Value for Email Secret
    People were complaining they didn't know how to
    set up the API because an error was being thrown.
    This prevents the error from being thrown.

--- config/email.js
+++ config/email.js
@@ -1,6 +1,8 @@
 export default {
   mailgun: {
-    secret: process.env.MAILGUN_SECRET,
+    // NOTE: 'test' prevents 'mailgun-js' from complaining that no secret has been set
+    // TODO: Read in environment variables from a '.env' file so we can set these default values in development
+    secret: process.env.MAILGUN_SECRET || 'test',
     domain: process.env.MAILGUN_DOMAIN || 'sse.rit.edu',
   },
 };
