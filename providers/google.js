a847e7d73c55930c751c80afb567703929f5639b
Gavriel R-H <gavriel@gavrielrh.com>
2019-08-23 22:10:40 -0400

    Update googleapis

--- providers/google.js
+++ providers/google.js
@@ -1,42 +1,48 @@
-import google from 'googleapis';
+import { google } from 'googleapis';
 import nconf from '../config';
 import User from '../models/user';
 
 const OAuth2 = google.auth.OAuth2;
 
 export default class GoogleProvider {
-
   constructor(token, dce) {
     this.dce = dce;
     this.authLevel = nconf.get('permissions:levels:high');
     this.token = token;
-    this.client = new OAuth2(nconf.get('auth:google:id'), nconf.get('auth:google:secret'));
+    this.client = new OAuth2(
+      nconf.get('auth:google:id'),
+      nconf.get('auth:google:secret')
+    );
   }
 
   verify() {
     return new Promise((resolve, reject) => {
-      // TODO: 'clientId' is 'undefined'; should be 'clientId_'
-      // This may change when upgrading 'googleapis'
-      this.client.verifyIdToken(this.token, this.client.clientId, (err, ticket) => {
-        if (err) {
-          return reject(err);
+      this.client.verifyIdToken(
+        {
+          idToken: this.token,
+          audience: this.client.clientId,
+        },
+        (err, ticket) => {
+          if (err) {
+            return reject(err);
+          }
+          this.payload = ticket.getPayload();
+          if (this.payload.hd === 'g.rit.edu') {
+            return resolve();
+          }
+          return reject({ message: 'Must login with a g.rit.edu account' });
         }
-        this.payload = ticket.getPayload();
-        if (this.payload.hd === 'g.rit.edu') {
-          return resolve();
-        }
-        return reject({ message: 'Must login with a g.rit.edu account' });
-      });
+      );
     });
   }
 
   findOrCreateUser() {
-    return User
-      .findOrCreate({ where: { dce: this.dce } })
-      .spread((user, created) => {
+    return User.findOrCreate({ where: { dce: this.dce } }).spread(
+      (user, created) => {
         user.firstName = this.payload.given_name;
         user.lastName = this.payload.family_name;
         return Promise.all([user.save(), created]);
-      });
+      }
+    );
   }
 }
--- routes/shifts.js
+++ routes/shifts.js
@@ -1,23 +1,31 @@
 import { Router } from 'express';
 import moment from 'moment';
-import google from 'googleapis';
+import { google } from 'googleapis';
 import nconf from '../config';
 
 const calendar = google.calendar('v3');
 const router = Router(); // eslint-disable-line new-cap
 
-router
-  .route('/')
-  .get((req, res, next) => {
-    if (req.accepts('json')) {
-      calendar.events.list({
+router.route('/').get((req, res, next) => {
+  if (req.accepts('json')) {
+    calendar.events.list(
+      {
         auth: nconf.get('auth:google:key'),
         calendarId: nconf.get('auth:google:calendars:mentor'),
         singleEvents: true,
-        timeMin: req.query.startTime || moment().day('Sunday').toISOString(),
-        timeMax: req.query.endTime || moment().day('Saturday').toISOString(),
+        timeMin:
+          req.query.startTime ||
+          moment()
+            .day('Sunday')
+            .toISOString(),
+        timeMax:
+          req.query.endTime ||
+          moment()
+            .day('Saturday')
+            .toISOString(),
         orderBy: 'startTime',
-      }, (err, response) => {
+      },
+      (err, response) => {
         if (err) {
           err.status = err.code;
           return next(err);
@@ -29,12 +37,20 @@ router
             fullName: summary,
           })),
         });
-      });
-    } else if (req.accepts('ics')) {
-      res.redirect(`https://calendar.google.com/calendar/ical/${nconf.get('auth:google:calendars:mentor')}/public/basic.ics`);
-    } else {
-      return next({ status: 406, message: `${req.headers.accept} is not acceptable` });
-    }
-  });
+      }
+    );
+  } else if (req.accepts('ics')) {
+    res.redirect(
+      `https://calendar.google.com/calendar/ical/${nconf.get(
+        'auth:google:calendars:mentor'
+      )}/public/basic.ics`
+    );
+  } else {
+    return next({
+      status: 406,
+      message: `${req.headers.accept} is not acceptable`,
+    });
+  }
+});
 
 export default router;
